6086e15a24d2455b0ab38db70288524a979dd8f8
95880 <95880@DESKTOP-CN0BS3E>
2019-10-08 20:52:31 +0800

    主页映射

--- src/main/java/com/example/demo/config/MyMvcConfig.java
+++ src/main/java/com/example/demo/config/MyMvcConfig.java
@@ -5,11 +5,14 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MyMvcConfig implements WebMvcConfigurer{
+public class MyMvcConfig implements WebMvcConfigurer {
 	@Override
-	 public void addViewControllers(ViewControllerRegistry registry) {
+	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/welcome").setViewName("index");
+		registry.addViewController("/").setViewName("login");
+		registry.addViewController("/index.html").setViewName("login");
+		registry.addViewController("/main.html").setViewName("dashboard");
 
 	}
-	
+
 }
